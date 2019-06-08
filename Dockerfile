d119970ad1e48be2a708ed2ec5a93d5fb177740a
Dmytro Nasyrov <dmytro@pharosproduction.com>
2019-06-08 16:40:14 +0300

    add run script and update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,34 @@
-# Build Geth in a stock Go builder container
 FROM golang:1.11-alpine as builder
+
+LABEL company="Pharos Production Inc."
+LABEL version="2.2.3"
+
+ENV LANG=C.UTF-8 \
+  REFRESHED_AT=2019-08-07-1
+ENV DEBIAN_FRONTEND noninteractive
 
 RUN apk add --no-cache make gcc musl-dev linux-headers git
 
-ADD . /go-ethereum
+ADD ./quorum /go-ethereum
 RUN cd /go-ethereum && make geth bootnode
 
-# Pull Geth into a second stage deploy alpine container
-FROM alpine:latest
+#############################################################
 
-RUN apk add --no-cache ca-certificates
+FROM alpine:3.9.4
+
+LABEL company="Pharos Production Inc."
+LABEL version="2.2.3"
+
+ENV LANG=C.UTF-8 \
+  REFRESHED_AT=2019-08-07-1
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apk add --no-cache ca-certificates bash
+
 COPY --from=builder /go-ethereum/build/bin/geth /usr/local/bin/
 COPY --from=builder /go-ethereum/build/bin/bootnode /usr/local/bin/
 
+COPY ./scripts/quorum.sh /opt/blockchain/quorum/
+RUN chmod +x /opt/blockchain/quorum/quorum.sh
 
-EXPOSE 8545 8546 30303 30303/udp
-ENTRYPOINT ["geth"]
+CMD [ "/bin/bash" ]
---
+++ scripts/quorum.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# UDS_WAIT=10
+
+# while getopts "t:n:" arg; do
+#   case $arg in
+#     t)
+#       TESSERA=$OPTARG
+#       ;;
+
+#     n)
+#       NODE_ID=$OPTARG
+#       ;;
+#   esac
+# done
+
+# for i in {1..50}
+# do  
+#   set -e
+#   if [ "$(wget2 --http2 --timeout ${UDS_WAIT} -qO- $TESSERA:9000/upcheck)" == "I'm up!" ]; then echo "tessera is up" && break
+#   else
+#     echo "Sleep ${UDS_WAIT} seconds. Waiting for Tessera at $TESSERA/upcheck."
+#     sleep ${UDS_WAIT}
+#   fi
+# done
+
+if ! [ -d /opt/blockchain/quorum/qdata/geth/lightchaindata ]; then
+  geth --datadir /opt/blockchain/quorum/qdata init /opt/blockchain/quorum/genesis.json
+  # sleep 10s
+fi
+
+
+# PRIVATE_CONFIG=/opt/blockchain/tessera/qdata/tm.ipc
+PRIVATE_CONFIG=ignore nohup geth --identity node$NODE_ID --verbosity 5 --networkid 10 \
+  --datadir /opt/blockchain/quorum/qdata \
+  --nodiscover --permissioned \
+  --raft --raftport 50400 --port 21000 \
+  --ws --wsaddr 0.0.0.0 --wsport 8546 --wsorigins "*" --wsapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft,rpc \
+  --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpccorsdomain "*" --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft,rpc \
+  --emitcheckpoints --unlock 0 --password /opt/blockchain/quorum/passwords 2> /opt/blockchain/quorum/logs/geth.log
